7c772aa2354dd43d48684f58119596f5605b778f
fawl_03 <razidias@gmail.com>
2022-08-17 10:19:09 -0300

    Começando projeto att 1.0.0

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <header>
+        <p>TrybeTunes</p>
+      </header>
+      
+    );
   }
 }
 
