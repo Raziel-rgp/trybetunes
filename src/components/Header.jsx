128efe490860cd0132eff7c57ded2c674813d16d
fawl_03 <razidias@gmail.com>
2022-08-24 13:21:07 -0300

    att 1.4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
+// log de atualização(1.4);
+// * adicionado test id
+
 export default class Header extends Component {
   // userName vai receber os dados retornados pela função getUser
   // loading vai mudar de estado para false quando acabar a requisição do nome do usuário.
@@ -32,10 +35,10 @@ export default class Header extends Component {
         { loading ? (<div><Loading /></div>) : (
           <div>
             <p data-testid="header-user-name">{ userName }</p>
-            <Link to="/search">Procurar </Link>
+            <Link data-testid="link-to-search" to="/search">Procurar </Link>
             <Link to="/album/:id"> album </Link>
-            <Link to="/favorites"> Favoritos </Link>
-            <Link to="/profile"> Perfil </Link>
+            <Link data-testid="link-to-favorites" to="/favorites"> Favoritos </Link>
+            <Link data-testid="link-to-profile" to="/profile"> Perfil </Link>
             <Link to="profile/edit"> Editar Perfil </Link>
           </div>
         ) }
